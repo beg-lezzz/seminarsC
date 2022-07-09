d44974427e3b565a1d070bac1aa2f84e8fb578a7
malistov.artem <malistov.artem@russianpost.ru>
2022-07-10 00:46:10 +0300

    add SolutionTask005

--- SolutionTask005/Program.cs
+++ SolutionTask005/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Console.WriteLine("Введите число: ");
+// string? inputline = Console.ReadLine();
+//
+// if (inputline != null)
+// {
+//     int inputNumber = int.Parse(inputline);
+//     int startNumber = inputNumber * -1;
+//     string lineOutput = "";
+//     while (startNumber < inputNumber)
+//     {
+//         lineOutput = lineOutput + startNumber + ", ";
+//         startNumber++;
+//     }
+//
+//     lineOutput = lineOutput + inputNumber;
+//     Console.WriteLine(lineOutput);
+// }
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число: ");
+string? inputline = Console.ReadLine();
+
+if (inputline != null)
+{
+    int inputNumber = int.Parse(inputline);
+    int startNumber = 1;
+    string lineOutput = "0";
+    while (startNumber <= inputNumber)
+    {
+        lineOutput = startNumber * -1 + ", " + lineOutput + ", " + startNumber;
+        startNumber++;
+    }
+    
+Console.WriteLine(lineOutput);
+}
