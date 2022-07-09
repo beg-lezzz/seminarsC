4bfbcc09db1975f06815e0fd23fb1c27f40903c6
malistov.artem <malistov.artem@russianpost.ru>
2022-07-09 23:43:28 +0300

    add SolutionTask001

---
+++ SolutionTask001/Program.cs
@@ -0,0 +1,9 @@
+﻿string? inputline = Console.ReadLine();
+
+if (inputline != null)
+{
+    int inputnumber = int.Parse(inputline);
+    // int outnumber = inputnumber * inputnumber;
+    int outnumber = (int)Math.Pow(inputnumber,2);
+    Console.WriteLine(outnumber);
+}
