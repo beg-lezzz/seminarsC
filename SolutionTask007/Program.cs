49601f112e0ba88b25460b35f5bec69032a2e86b
malistov.artem <malistov.artem@russianpost.ru>
2022-07-10 01:04:14 +0300

    add SolutionTask007

---
+++ SolutionTask007/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+string? inputline = Console.ReadLine();
+
+if (inputline != null && inputline.Length == 3)
+{
+    // int inputNumber = int.Parse(inputline);
+    // int last = inputNumber%10;
+    // string last = inputline.Substring(2, 1);
+    char[] last = inputline.ToCharArray();
+    Console.WriteLine(last[2]);
+}
