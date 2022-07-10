60ce393bc8ba244c614a44c0a137315f816e4998
malistov.artem <malistov.artem@russianpost.ru>
2022-07-10 22:22:49 +0300

    add SolutionTask006

---
+++ SolutionTask006/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите число: ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    double inputNumber = double.Parse(inputLine);
+    double divResult = inputNumber / 2;
+    
+    if (divResult.ToString().Contains(","))
+    {
+        Console.WriteLine("Нет");
+    }
+    else
+    {
+        Console.WriteLine("Да");
+    }
+
+    // int inputNumber = int.Parse(inputLine);
+    //  if (inputNumber % 2 == 0)
+    //  {
+    //      Console.WriteLine("Да");
+    //  }
+    //  else
+    //  {
+    //      Console.WriteLine("Нет");
+    //  }
+}
