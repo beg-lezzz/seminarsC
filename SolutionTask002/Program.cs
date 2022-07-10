634d657b7580932944566aa06684cb46badcd048
malistov.artem <malistov.artem@russianpost.ru>
2022-07-10 11:40:59 +0300

    add SolutionTask002

---
+++ SolutionTask002/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите первое число: ");
+string? inputlineA = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string? inputlineB = Console.ReadLine();
+
+if (inputlineA != null && inputlineB != null)
+{
+    int inputNumberA = int.Parse(inputlineA);
+    int inputNumberB = int.Parse(inputlineB);
+    int max = inputNumberA;
+        
+    if (max == inputNumberB)
+    {
+        Console.WriteLine("Числа равны");
+    }
+    else
+    {
+        if (max < inputNumberB)
+        {
+            max = inputNumberB;
+        }
+        
+        Console.Write("max = " + max);
+    }
+}
