cd78c6990f11dd4965f22857b4849f2115e9ea9c
malistov.artem <malistov.artem@russianpost.ru>
2022-07-12 21:46:23 +0300

    add SolutionTask014

---
+++ SolutionTask014/Program.cs
@@ -0,0 +1,46 @@
+﻿void findDiv()
+{
+    Console.Write("Enter number: ");
+    string? firstLine = Console.ReadLine();
+    int first = int.Parse(firstLine);
+
+
+    if (first%7 == 0 && first%23 == 0)
+    {
+        Console.WriteLine("Кратное");
+    }
+    else
+    {
+        Console.WriteLine("Не кратное");
+    }    
+}
+
+findDiv();
+
+Console.WriteLine("Input number: ");
+
+string? inputOne = Console.ReadLine();
+
+string reminderSearcher(string str)
+{
+    if (inputOne != null)
+    {
+        try {
+            string answer;
+            int num = int.Parse(str);
+            if (((num % 7) == 0) && ((num % 23) == 0)){
+                answer = "It's multiple for 7 and 23";
+            } else {
+                answer = "It's not multiple for 7 and 23";
+            }
+            return answer;
+        }
+        catch(Exception e)
+        {
+            return "Возникло исключение: " + e.Message;
+        }
+    }
+    return "";
+}
+
+Console.WriteLine(reminderSearcher(inputOne));
