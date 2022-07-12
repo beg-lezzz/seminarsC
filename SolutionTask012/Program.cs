df65a9d16b61c37833d6f6f3c6e8618fb26ca309
malistov.artem <malistov.artem@russianpost.ru>
2022-07-12 21:31:56 +0300

    add SolutionTask012

---
+++ SolutionTask012/Program.cs
@@ -0,0 +1,46 @@
+﻿// Console.Write("Enter first number: ");
+// string? firstLine = Console.ReadLine();
+// Console.Write("Enter second number: ");
+// string? secondLine = Console.ReadLine();
+// int first = int.Parse(firstLine);
+// int second = int.Parse(secondLine);
+//
+// if (second%first == 0)
+// {
+//     Console.WriteLine("Кратное");
+// }
+// else
+// {
+//     Console.WriteLine("Не кратное, " + second%first);
+// }
+
+Console.WriteLine("Input two numbers: ");
+
+string? inputOne = Console.ReadLine();
+string? inputTwo = Console.ReadLine();
+
+string reminderSearcher(string str1, string str2)
+{
+    if (inputOne != null && inputTwo != null)
+    {
+        try {
+            string answer;
+            int num1 = int.Parse(str1);
+            int num2 = int.Parse(str2);
+            int remainder = num1 % num2;
+            if (remainder != 0){
+                answer = "The remainder is : " + remainder;
+            } else {
+                answer = "It's multiple!!!";
+            }
+            return answer;
+        }
+        catch(Exception e)
+        {
+            return "Возникло исключение: " + e.Message;
+        }
+    }
+    return "";
+}
+
+Console.WriteLine(reminderSearcher(inputOne,inputTwo));
