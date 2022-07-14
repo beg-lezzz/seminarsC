ae3f343f38751cfd3548777fff0d795e4118b969
malistov.artem <malistov.artem@russianpost.ru>
2022-07-14 22:52:08 +0300

    add SolutionTask010

---
+++ SolutionTask010/Program.cs
@@ -0,0 +1,70 @@
+﻿Console.Write("Введите трёхзначное число: ");
+string? firstLine = Console.ReadLine(); 
+bool answer = true;
+
+bool nullCheck(string str)
+{
+    if (str == null || str == "")
+    {
+        answer = false;
+    }
+    return answer;
+}
+    
+bool threeDigitCheck(string str)
+{
+    if (firstLine.Length != 3)
+    {
+        answer = false;
+    }
+    return answer;
+}
+    
+bool zeroCheck(string str)
+{
+    char[] array = firstLine.ToString().ToCharArray();
+    int firstDig = ((int)array[0]) - 48;
+    if (firstDig == 0)
+    {
+        answer = false;
+    }
+    return answer;
+}
+
+bool intCheck(string str)
+{
+    int num;
+    bool isNum = int.TryParse(firstLine, out num);
+    return isNum;
+}
+
+if (nullCheck(firstLine) == true)
+{
+    if (intCheck(firstLine))
+    {
+        if (threeDigitCheck(firstLine) == true)
+        {
+            if (zeroCheck(firstLine) == true)
+            {
+                int secondDigit = int.Parse(firstLine);
+                Console.WriteLine(secondDigit);
+            }
+            else
+            {
+                Console.WriteLine("Число не может начинаться с нуля");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Введенное число не трёхзначное");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не число");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ничего не ввели");;
+}
