e52732da7f58e40d435666a770697493bb8f92bb
malistov.artem <malistov.artem@russianpost.ru>
2022-07-15 01:14:49 +0300

    add SolutionTask015

---
+++ SolutionTask015/Program.cs
@@ -0,0 +1,36 @@
+﻿int randomNum = new Random().Next(1,8);
+Console.WriteLine(randomNum);
+string? answer;
+
+var weekDaysDict = new Dictionary<int, bool>()
+{
+    {1, false},
+    {2, false},
+    {3, false},
+    {4, false},
+    {5, false},
+    {6, true},
+    {7, true},
+};
+
+if (weekDaysDict[randomNum] == false)
+{
+    answer = "Рабочий";
+}
+else
+{
+    answer = "Выходной";
+}
+
+Console.WriteLine("В словаре это - " + answer);
+
+var weekDayVacations = new List<int>(){6,7};
+
+if (weekDayVacations.Contains(randomNum) == true)
+{
+    Console.WriteLine("В списке это - Выходной");
+}
+else
+{
+    Console.WriteLine("В списке это - Рабочий");
+}
