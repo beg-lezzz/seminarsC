b4b4597f188e9744d378f9f0b0300d1fe5912092
malistov.artem <malistov.artem@russianpost.ru>
2022-07-21 16:17:12 +0300

    add SolutionTask022

---
+++ SolutionTask022/Program.cs
@@ -0,0 +1,54 @@
+﻿// Console.WriteLine("Введите число: ");
+// string? inputLineN = Console.ReadLine();
+//
+// if (inputLineN != null)
+// {
+//     int number = int.Parse(inputLineN);
+//     string? lineN = "";
+//     string? lineNN = string.Empty;
+//     int counter = 1;
+//
+//     while (counter < number + 1)
+//     {
+//         lineN = lineN + counter + " ";
+//         lineNN = lineNN + counter * counter + " ";
+//
+//         counter++;
+//     }
+//     
+//     Console.WriteLine(lineN);
+//     Console.WriteLine(lineNN);
+// }
+
+// Вариант Кирилла
+
+List<int> listGen(int number)
+{
+    List<int> numbers = new List<int>();
+    for (int i = 1; i < number + 1; i++)
+    {
+        numbers.Add(i);
+    }
+
+    return numbers;
+}
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    List<int> nums = listGen(inputNumber);
+    foreach (var i in nums)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+
+    foreach (var i in nums)
+    {
+        Console.Write(Math.Pow(i,2));
+        Console.Write(" ");
+    }
+}
