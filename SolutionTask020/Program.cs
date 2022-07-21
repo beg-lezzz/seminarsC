12b184cf357b7c35cafeb9760add3c0eec7d9ed3
malistov.artem <malistov.artem@russianpost.ru>
2022-07-21 15:47:45 +0300

    add SolutionTask020

---
+++ SolutionTask020/Program.cs
@@ -0,0 +1,31 @@
+﻿int aX;
+int aY;
+int bX;
+int bY;
+double varAB;
+
+void readDataOfPoint()
+{
+    Console.Write("Введите координату Х точки А: ");
+    aX = int.Parse(Console.ReadLine());
+    
+    Console.Write("Введите координату Y точки А: ");
+    aY = int.Parse(Console.ReadLine());
+    
+    Console.Write("Введите координату Х точки B: ");
+    bX = int.Parse(Console.ReadLine());
+    
+    Console.Write("Введите координату Y точки B: ");
+    bY = int.Parse(Console.ReadLine());    
+}
+
+
+void calculateVarAB()
+{
+    varAB = Math.Sqrt(Math.Pow((aX - bX),2) + Math.Pow((aY - bY),2));    
+}
+
+readDataOfPoint();
+calculateVarAB();
+
+Console.WriteLine("Расстояние между точками = " + varAB);
