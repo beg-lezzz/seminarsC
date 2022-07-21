8aab9a97ea7c68e98a301398b18298848be9b915
malistov.artem <malistov.artem@russianpost.ru>
2022-07-21 15:29:43 +0300

    add SolutionTask018

---
+++ SolutionTask018/Program.cs
@@ -0,0 +1,18 @@
+﻿void printAnswer(int number)
+{
+    if (number == 1) Console.WriteLine("Допустимо: x > 0; y > 0");
+    if (number == 2) Console.WriteLine("Допустимо: x < 0; y > 0");
+    if (number == 3) Console.WriteLine("Допустимо: x < 0; y < 0");
+    if (number == 4) Console.WriteLine("Допустимо: x > 0; y < 0"); 
+}
+
+
+Console.WriteLine("Введите номер четверти:");
+string? inputline = Console.ReadLine();
+
+if (inputline != null)
+{
+    int inputNumber = int.Parse(inputline);
+    
+    printAnswer(inputNumber);
+}
