ae0afcb7fbf8f8af27af73f4db3e8d58eb89691f
malistov.artem <malistov.artem@russianpost.ru>
2022-08-02 21:08:31 +0300

    add SolutionTask026

---
+++ SolutionTask026/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.Write("Введите число: ");
+
+string inputLineNumber = Console.ReadLine()??"";
+int inputNumber = int.Parse(Console.ReadLine());
+int t;
+
+DateTime timePoint = DateTime.Now;
+
+int VariantChar()
+{
+    // int numberLength = 0;
+    //
+    // char[] array = inputLineNumber.ToCharArray();
+    // numberLength = array.Length;
+    return inputLineNumber.ToCharArray().Length;
+}
+
+int VariantSimple()
+{
+    int numberLength = 0;
+    int digits = 1;
+
+    while (digits < inputNumber)
+    {
+        digits = digits * 10;
+        numberLength++;
+    }
+
+    return numberLength;
+}
+
+int VariantLog10()
+{
+    return (int)Math.Log10(inputNumber) + 1;
+}
+
+int result = 0;
+
+result = VariantChar();
+Console.WriteLine(result);
+Console.WriteLine(DateTime.Now - timePoint);
+
+timePoint = DateTime.Now;
+
+result = VariantSimple();
+Console.WriteLine(result);
+Console.WriteLine(DateTime.Now - timePoint);
+
+timePoint = DateTime.Now;
+
+result = VariantLog10();
+Console.WriteLine(result);
+Console.WriteLine(DateTime.Now - timePoint);
