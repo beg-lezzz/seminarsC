159d8a991f7a34c183b2d559c26c26e0848baf3e
malistov.artem <malistov.artem@russianpost.ru>
2022-08-02 20:44:20 +0300

    add remarks in SolutionTask024

---
+++ SolutionTask024/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите число: ");
+
+int inputNumber = int.Parse(Console.ReadLine());
+int t;
+
+void VariantSimple()
+{
+    int sumOfNumbers = 0;
+    for (int i = 1; i <= inputNumber; i++)
+    {
+        sumOfNumbers += i;
+    }
+    
+    Console.WriteLine("Сумма чисел от 1 до " + inputNumber + " = " + sumOfNumbers);
+}
+
+void VariantGauss()
+{
+    int sumOfNumbers = 0;
+
+    sumOfNumbers = ((inputNumber + 1) * inputNumber) / 2;
+    
+    Console.WriteLine("Сумма чисел от 1 до " + inputNumber + " = " + sumOfNumbers);
+}
+
+t = Environment.TickCount;
+
+VariantSimple();
+
+Console.WriteLine("Время выполнения: {0} мс", Environment.TickCount - t);
+
+VariantGauss();
