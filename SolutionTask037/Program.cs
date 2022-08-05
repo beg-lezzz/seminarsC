5311c2dc572310feb58c9fad1c901bdeab11e258
malistov.artem <malistov.artem@russianpost.ru>
2022-08-05 22:00:21 +0300

    add remarks in SolutionTask037

--- SolutionTask037/Program.cs
+++ SolutionTask037/Program.cs
@@ -1,29 +1,29 @@
 ﻿int[] FillArray()
 {
-    int[] inputArray = new int[123];
+    int[] inputArray = new int[11];
     int i = 0;
     System.Random numberSyntezator = new System.Random();
-    while (i < 123)
+    while (i < 11)
     {
-        inputArray[i] = numberSyntezator.Next(-1000,1000);
+        inputArray[i] = numberSyntezator.Next(-10,10);
         i++;
     }
 
     return inputArray;
 }
 
-void PrintArray(int[] outArray)
+void PrintArray(int[] inputArray)
 {
-    for (int i = 0; i < outArray.Length - 1; i++)
+    for (int i = 0; i < inputArray.Length - 1; i++)
     {
-        Console.Write(outArray[i] + ", ");
+        Console.Write(inputArray[i] + ", ");
     }
-    Console.WriteLine(outArray[outArray.Length - 1]);
+    Console.WriteLine(inputArray[inputArray.Length - 1]);
 }
 
 int[] CalculateTask(int[] inputArray)
 {
-    int[] resultArray = new int[inputArray.Length / 2 + 1];
+    int[] resultArray = new int[inputArray.Length / 2];
     int i = 0;
 
     while (i < resultArray.Length)
@@ -36,8 +36,6 @@ int[] CalculateTask(int[] inputArray)
 
 int[] bufferArray = FillArray();
 PrintArray(bufferArray);
-Console.WriteLine("------");
-Console.WriteLine("------");
-Console.WriteLine("------");
+Console.WriteLine();
 int[] resultArray = CalculateTask(bufferArray);
 PrintArray(resultArray);
