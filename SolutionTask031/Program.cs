f5d9318b3767fdfc6dcdcc38e9144d9063d88716
malistov.artem <malistov.artem@russianpost.ru>
2022-08-05 20:58:33 +0300

    add SolutionTask031

---
+++ SolutionTask031/Program.cs
@@ -0,0 +1,59 @@
+﻿int positSum = 0;
+int negatSum = 0;
+
+int[] FillArray()
+{
+    int[] outArray = new int[12];
+    int i = 0;
+    System.Random numberSyntezator = new System.Random();
+    while (i < 12)
+    {
+        outArray[i] = numberSyntezator.Next(-9, 10);
+        i++;
+    }
+
+    return outArray;
+}
+
+void PrintArray(int[] outArray)
+{
+    for (int i = 0; i < outArray.Length - 1; i++)
+    {
+        Console.Write(outArray[i] + ", ");
+    }
+    Console.WriteLine(outArray[outArray.Length - 1]);
+}
+
+void CalculateTask(int[] outArray)
+{
+    int i = 0;
+    while (i < 12)
+    {
+        if (outArray[i] > 0)
+        {
+            positSum = positSum + outArray[i];
+        }
+        else
+        {
+            negatSum = negatSum + outArray[i];
+        }
+
+        i++;
+    }
+}
+
+void PrintResult()
+{
+    Console.WriteLine(positSum);
+    Console.WriteLine(negatSum);
+}
+
+void VariantNaive()
+{
+    int[] bufferArray = FillArray();
+    PrintArray(bufferArray);
+    CalculateTask(bufferArray);
+    PrintResult();
+}
+
+VariantNaive();
