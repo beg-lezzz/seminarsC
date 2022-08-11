ce7a7276b4ad4f3d1ac194fc9ee5fe73044c371c
malistov.artem <malistov.artem@russianpost.ru>
2022-08-11 16:11:52 +0300

    add remarks in SolutionTask034

--- SolutionTask034/Program.cs
+++ SolutionTask034/Program.cs
@@ -39,18 +39,17 @@ void PrintArray(int[] inputArray)
     Console.WriteLine(inputArray[inputArray.Length - 1]);
 }
 
-// сортировка методом пузырька с печатью первоначального массива
-int[] Bubble(int[] inputArray)
+// сортировка методом пузырька
+int[] BubbleSortArray(int[] inputArray)
 {
-    Console.WriteLine("Сортировка методом пузырька: ");
     int[] outputArray = new int[inputArray.Length];
     int tmp;                                                        // задаем буферную переменную
-    int k = 0;                                                      // задаем счетчик количества проходов внешнего цикла, чтобы уменьшать верхнюю границу, т.к. после каждого прохода
+    int k = inputArray.Length - 1;                                                      // задаем счетчик количества проходов внешнего цикла, чтобы уменьшать верхнюю границу, т.к. после каждого прохода
                                                                     // максимальный элемент оказывается в конце и к нему уже не нужно возвращаться
                                                                     
     for (int i = 0; i < inputArray.Length; i++)                     // запускаем внешний цикл для того, чтобы пройти последовательно по всем элементам массива
     {
-        for (int j = 0; j < inputArray.Length - 1 - k; j++)         // запускаем внутренний цикл для того, чтобы последовательно сравнивать элементы массива в каждой итеррации внешнего цикла
+        for (int j = 0; j < k; j++)         // запускаем внутренний цикл для того, чтобы последовательно сравнивать элементы массива в каждой итеррации внешнего цикла
         {
             if (inputArray[j] > inputArray[j + 1])                  // условие для "перетаскивания" вправо максимального элемента
             {
@@ -60,7 +59,7 @@ int[] Bubble(int[] inputArray)
             }
         }
 
-        k++;
+        k--;
     }
     return inputArray;
 }
@@ -73,12 +72,14 @@ if (Int32.TryParse(input, out var arrLength) == true)
     int[] inputArray = new int[arrLength];
     inputArray = FillArray(arrLength);
     Console.WriteLine();
-    Console.WriteLine("Исходный массив: ");
+    //Console.WriteLine("Исходный массив: ");
     PrintArray(inputArray);
     Console.WriteLine();
-    Console.WriteLine($"Количество четных элементов в массиве: {EvenCount(inputArray)}");
+    Console.WriteLine($"Количество четных элементов в массиве:" +
+                      $"{EvenCount(inputArray)}");
     Console.WriteLine();
-    PrintArray(Bubble(inputArray));
+    Console.WriteLine("Сортировка методом пузырька: ");
+    PrintArray(BubbleSortArray(inputArray));
 }
 else
 {
