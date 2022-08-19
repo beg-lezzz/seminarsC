42bc0f52fac9043020bdf6c3e88744b68de38b9e
malistov.artem <malistov.artem@russianpost.ru>
2022-08-19 20:58:59 +0300

    add SolutionTask063

---
+++ SolutionTask063/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int ReadData()
+{
+    Console.WriteLine("Задайте число: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int NaturalNumberPrinter(int num)
+{
+    if (num == 2) return 1;
+    else
+    {
+        num--;
+        Console.WriteLine(num);
+        Console.Write(NaturalNumberPrinter(num) + " ");
+    }
+
+    return num;
+}
+
+int inputData = ReadData();
+NaturalNumberPrinter(inputData + 2);
