9d555516e62ee1d64652acd7335e49a42b41cd14
malistov.artem <malistov.artem@russianpost.ru>
2022-08-19 21:38:56 +0300

    add SolutionTask067

---
+++ SolutionTask067/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+int number = ReadData("Задайте число: ");
+int sum = 0;
+
+Console.WriteLine(Sum(number));
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+int Sum(int num)
+{
+    if (num > 0)
+    {
+        return num % 10 + Sum(num / 10);
+    }
+    else
+    {
+        return 0;
+    }
+}
