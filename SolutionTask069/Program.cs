7e66a05eb42223a2cff92a92e94f9a8aa694cf4b
malistov.artem <malistov.artem@russianpost.ru>
2022-08-19 21:49:25 +0300

    add SolutionTask069

---
+++ SolutionTask069/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+int numberN = ReadData("Задайте число M: ");
+int numberM = ReadData("Задайте число N: ");
+int sum = 0;
+
+// if (numberM < numberN)
+// {
+//     Console.WriteLine(PowNumber(numberM, numberN));
+// }
+// else
+// {
+//     Console.WriteLine(PowNumber(numberN, numberM));
+// }
+
+Console.WriteLine(PowNumber(numberM, numberN));
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+int PowNumber(int m, int n)
+{
+    if (n == 1) return m;
+    return m * PowNumber(m, --n);
+}
